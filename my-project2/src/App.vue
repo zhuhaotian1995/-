fbb1247443dcb5dc3155a5593e38e0c0b51ddd92
zhuhaotian1995 <haotian12.zhu@gmail.com>
2020-10-26 13:38:36 -0400

    comment function

---
+++ my-project2/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+  <header class="site-header jumbotron">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <h1>我是iphone12</h1>
+        </div>
+      </div>
+    </div>
+  </header>
+  <div class="container">
+    <Add></Add>
+    <List></List>
+  </div>
+
+</div>
+</template>
+
+<script>
+import List from './components/List'
+import Add from './components/Add'
+
+export default {
+  name: 'App',
+  components: {
+    List,
+    Add
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-project2/src/components/List.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="col-md-4">
+  <p>请添加评论</p>
+  <ul class="list-group">
+    <li class="list-group-item">
+      <div class="handle">
+        <a href="javascript:;">删除</a>
+        <p class="user"><span>yyy</span><span>说：</span></p>
+        <p class="centence">vue之神</p>
+      </div>
+    </li>
+    <li class="list-group-item">
+      <div class="handle">
+        <a href="javascript:;">删除</a>
+        <p class="user"><span>xxx</span><span>说：</span></p>
+        <p class="centence">vue king</p>
+      </div>
+    </li>
+  </ul>
+
+</div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.col-md-4 {
+  position: absolute;
+  right: 200px;
+  top: 220px;
+}
+
+.handle a {
+  width: 40px;
+  position: absolute;
+  right: 10px;
+  top: 1px;
+}
+</style>
